61972f206dd94fa4b9c20992ba03f1f7d4dd4b16
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-13 16:57:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -38,12 +38,12 @@
 
             $dbconn = pg_connect("host=ec2-50-19-124-157.compute-1.amazonaws.com port=5432 dbname=d58o173hbaukt7 user=nfltllotkrgbnc password=6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0");
 
-            $fName = pg_escape_string($_POST['name']);
+            $fName = pg_escape_string($_POST['Name']);
             
             
             if(!empty( $fName )){ 
 
-                $sql = "INSERT into contact.contact (name) values ('HHHHHHHHHHH')" ;
+                $sql = "INSERT into contact.contact (Name) values ('HHHHHHHHHHH')" ;
                 pg_query($dbconn, $sql); 
          
     ?> 
