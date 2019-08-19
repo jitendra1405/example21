a99603fd48e80619658ed2bcf86d6cb509c9bd67
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-19 13:00:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,38 +29,45 @@
         </ul>
     </div> 
 
-    <?php
-    
-       
+    <?
+        /*
+            Sample Database String from Heroku
+            'postgres://wvvgxgeoriumxg:c4e8612ae286a211a8c94976df0811e9b6fcdacb3ef3e468401e0619b38a1004@ec2-107-22-168-211.compute-1.amazonaws.com:5432/d5siauekbh9qlu' 
+            */
 
             $dbconn = pg_connect("host=ec2-50-19-124-157.compute-1.amazonaws.com port=5432 dbname=d58o173hbaukt7 user=nfltllotkrgbnc password=6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0");
 
-             $sql = "INSERT into contact.contact (FirstName) values ('AAAAAAAAAAAAAAAAFFrrooo')" ;
-                pg_query($dbconn, $sql); 
+            $fName = pg_escape_string($_POST['name']);
+            
+            
+            if(!empty( $name )){ 
 
-         
+                $sql = "INSERT into contact.contact (name) values ('".$fName."')" ;
+                pg_query($dbconn, $sql); 
     ?> 
             <div class="alert alert-success" role="alert">
                 Record Inserted Succesfully in Heroku Postgres Database !!!
             </div> 
-    <?
+            <?
                 //close sql if statement bracket
                 }
             ?>
      
-           
      <div class="row">
          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
              
             <form method="post">
                 <div class="form-group">
-                    <label for="firstName">Last Name</label>
-                    <input  type="tetxt" class="form-control" name="firstName" id="firstName" placeholder="First Name" >
+                    <label for="firstName">First Name</label>
+                    <input required="true" type="tetxt" class="form-control" name="firstName" id="firstName" placeholder="First Name">
                 </div>
 
-               
+                <div class="form-group">
+                    <label for="lastName">Last Name</label>
+                    <input  required="true" type="tetxt" class="form-control" name="lastName" id="lastName" placeholder="Last Name">
+                </div>
                  
-                <button type="submit" class="btn btn-primary" >Submit</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
 
          </div>
@@ -69,8 +76,8 @@
             <table class="table table-striped table-hover table-bordered">
                 <thead>
                     <tr>
-                        
-                        
+                        <th>Key</th>
+                        <th>First Name</th>
                         <th>Last Name</th>
                     </tr>
                 </thead>
@@ -95,7 +102,7 @@
                             pg_query($dbconn, $sql);
                             */ 
 
-                            $sql = "select firstname from contact.contact";
+                            $sql = "select name from contact.contact";
 
                             $resultset = pg_query($dbconn, $sql);
                             while($row = pg_fetch_array($resultset)) {
