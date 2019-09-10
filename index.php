e1e2e36595c318135e4c8c7ca3880c8ebed6cd60
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-10 16:23:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,11 @@
 <!DOCTYPE html>
 <html>
-   <title>User Login</title>
-   <!--/////////////////Head Include/////////-->
-   <?php include 'header.php';?>
-   <!--/////////////////////////////////////-->
+   
    <body class="login">
-      <section id="login-section">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-6 col-sm-12 col-lg-6 login-form">
-                  <div id="login-container">
-                     <form action="" method="post" role="form" class="contactForm">
-                           <div class="form-group">
-				   
-                           <input type="email" class="form-control login-form-input" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-				   
-					   <div class="validation"></div>
-                        </div>
-					
-                        <div class="form-group">
-                           <input type="password" class="form-control login-form-input" name="password" id="password" placeholder="Password" data-rule="minlen:4" data-msg="Please enter correct password" />
-                           <div class="validation"></div>
-                        </div>
-						
-						<div class="form-check form-check-inline">
-						  <input class="form-check-input" type="checkbox" id="KeepSignin" value="Keep me sign in">
-						  <label class="form-check-label" for="KeepSignin">Keep me sign in</label>
-						</div>
-						
-						<form action="theSamePage.php" method="post">
+      
+				<form action="theSamePage.php" method="post">
     								<input type="submit" name="someAction" value="GO" />
-						</form>
-                     </form>
-
-                  </div>
-               </div>
-               <div class="col-md-6 col-sm-12 col-lg-6 login-form-banner">
-               </div>
-            </div>
-         </div>
-      </section>
+		
 	 <?php
 		if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
     		{
