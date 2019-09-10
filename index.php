97084ea651fa25acedc776f8a5a4c6978ceafe2c
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-10 16:31:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html>
-   
-   <body class="login">
-      
-				<form action="index.html" method="post">
-    								<input type="submit" name="someAction" value="GO" />
-		
+
+				
 	 <?php
-		if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
-    		{
-        		abc();
-		}
+		
 	   function abc(){
 		   	   $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
 			
@@ -30,5 +21,4 @@
                             pg_close($dbconn); 
 	   }
 ?>
-   </body>
-</html>
+  
