a16c9b0a009af332d5b01636c15d1742eec33711
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-10 15:53:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,30 +29,11 @@
         </ul>
     </div> 
 
-    <?
-        /*
-            Sample Database String from Heroku
-            'postgres://wvvgxgeoriumxg:c4e8612ae286a211a8c94976df0811e9b6fcdacb3ef3e468401e0619b38a1004@ec2-107-22-168-211.compute-1.amazonaws.com:5432/d5siauekbh9qlu' 
-            */
+    <?   
+    $dbconn = pg_connect("host=ec2-23-21-156-171.compute-1.amazonaws.com port=5432 dbname=daff54nelb3ps6 user=leeglxtkajgvtl password=76f29beea03eb3bd5b69672f0d292a01ae95d251957282df96e882864c969e50");
 
-            $dbconn = pg_connect("host=ec2-50-19-124-157.compute-1.amazonaws.com port=5432 dbname=d58o173hbaukt7 user=nfltllotkrgbnc password=6501404a979c7d7bb1da09c71cd54a6b83fb4986354072611d25bf3c0f0287e0");
-
-            $fName = pg_escape_string($_POST['name']);
-            
-            
-            if(!empty( $name )){ 
-
-                $sql = "INSERT into contact.contact (FirstName) values ('".$fName."')" ;
-                pg_query($dbconn, $sql); 
-    ?> 
-            <div class="alert alert-success" role="alert">
-                Record Inserted Succesfully in Heroku Postgres Database !!!
-            </div> 
-            <?
-                //close sql if statement bracket
-                }
-            ?>
-     
+           
+     ?>
      <div class="row">
          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
              
@@ -82,27 +63,9 @@
                     </tr>
                 </thead>
                 <tbody> 
-                        <?    
-                            /*
-                            Use below code to create table and insert some sample records if using first time
-                            $sql = "drop table Person;";
-                            pg_query($dbconn, $sql);
-                            
-                            $sql = "create table Person (id SERIAL PRIMARY KEY, firstName varchar(15), lastName varchar(15) );";
-                            pg_query($dbconn, $sql);
-                            
+                        
 
-                            $sql = "INSERT into Person (firstName,lastName) values ('Rudra', 'Zaa')" ;
-                            pg_query($dbconn, $sql);
-
-                            $sql = "INSERT into Person (firstName,lastName) values ('Shivanya', 'Zaa')" ;
-                            pg_query($dbconn, $sql);
-
-                            $sql = "INSERT into Person (firstName,lastName) values ('Minal', 'Zaa')" ;
-                            pg_query($dbconn, $sql);
-                            */ 
-
-                            $sql = "select FirstName from contact.contact";
+                            $sql = "SELECT email,lastname FROM webrtc.contact";
 
                             $resultset = pg_query($dbconn, $sql);
                             while($row = pg_fetch_array($resultset)) {
